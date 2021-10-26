78082da3b6458d48d9768a582431c78a79c46e10
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-10-26 16:10:03 +0545

    Adding: Login

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
-
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,6 +19,9 @@ function App() {
       <Router>
         <Header/>
         <Switch>
+              <Route path="/login">
+                    <Login/>
+              </Route>
               <Route path="/detail">
                     <Detail/>
               </Route>
